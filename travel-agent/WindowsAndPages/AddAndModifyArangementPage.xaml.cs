209c841bd64f87190624e333f1b325e5c42c3346
Danica Gazdić <danicagazdic@gmail.com>
2023-06-05 14:09:40 +0200

    Started work on AddModifyArrangement page

--- travel-agent/WindowsAndPages/AddAndModifyArangementPage.xaml.cs
+++ travel-agent/WindowsAndPages/AddAndModifyArangementPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using travel_agent.Models;
+using travel_agent.Services;
 
 namespace travel_agent.WindowsAndPages
 {
@@ -23,12 +25,66 @@ namespace travel_agent.WindowsAndPages
 	{
 		private new readonly MainWindow Parent;
 		private Application App;
+		private ArrangementService ArrangementService;
+		private PlaceService PlaceService;
+		private BitmapImage Image = null;
+		private Arrangement Arrangement;
 
 		public AddAndModifyArangementPage(MainWindow parent, Arrangement arrangement = null)
 		{
 			InitializeComponent();
 			App = Application.Current;
 			Parent = parent;
+			ArrangementService = ArrangementService.Instance;
+			PlaceService = PlaceService.Instance;
+			Arrangement = arrangement;
+			SetUpPage();
+
 		}
+
+		private List<Place> FilterPlaces()
+		{
+			List<Place> allPlaces = PlaceService.GetAll();
+			foreach (var place in allPlaces) { Console.WriteLine(place); }
+			if(Arrangement == null) return allPlaces;
+			List<Place> places = (from place in allPlaces where !Arrangement.Places.Contains(place) select place ).ToList();
+			return places;
+		}
+
+		private void SetUpPage()
+		{
+			List<Place> places = this.FilterPlaces(); 
+			if(places.Count == 0) {
+				// TODO setup no content
+			}
+			AllPlacesLI.ItemsSource = places;
+			if (Arrangement == null) return;
+			ArrangementNameInput.InputText = Arrangement.Name;
+			SetImage(Arrangement.Image);
+			StartDatePicker.SelectedDate = Arrangement.Start;
+			EndDatePicker.SelectedDate = Arrangement.End;
+			
+		}
+
+		private void SetImage(BitmapImage image)
+		{
+			Image = image;
+			var imageBrush = new ImageBrush(Image);
+			imageBrush.Stretch = Stretch.UniformToFill;
+			imageBrush.AlignmentX = AlignmentX.Center;
+			imageBrush.AlignmentY = AlignmentY.Center;
+			PlaceImage.Background = imageBrush;
+			PlaceImageLabel.Visibility = Visibility.Hidden;
+			PlaceAddImageButton.Content = App.Resources["String.SwichImageButton"] as string;
+		}
+
+		private void OnAddPictureClick(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Image Files (*.png;*.jpeg;*.jpg;*.gif;*.bmp)|*.png;*.jpeg;*.jpg;*.gif;*.bmp";
+			if (openFileDialog.ShowDialog() == true) SetImage(new BitmapImage(new Uri(openFileDialog.FileName)));
+		}
+
+		private void OnBackClick(object sender, RoutedEventArgs e) => Parent.MainFrame.Content = new PlacesPage(Parent);
 	}
 }
