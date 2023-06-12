3a7a717d9f3e6f01fdb1bb9f7888e748dddd083b
Danica Gazdić <danicagazdic@gmail.com>
2023-06-12 16:23:05 +0200

    Html settup

---
+++ travel-agent/Help/Files/script.js
@@ -0,0 +1,11 @@
+﻿function changeContent(columnName) {
+    var selectedColumn = document.getElementById(columnName);
+    var content = document.getElementsByClassName("content");
+
+    // Perform logic to change the content based on the selected column
+    for (i = 0; i < content.length; i++) {
+        content[i].classList.add("dissapear_content");
+    }
+    selectedColumn.classList.remove("dissapear_content");
+    
+}
